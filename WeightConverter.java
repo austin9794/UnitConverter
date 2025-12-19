6619ecf033b085207795358f0dbccd4523ae0a7d
austin9794 <mohdumair72914@gmail.com>
2025-12-19 16:29:33 +0000

    weight logic

--- WeightConverter.java
+++ WeightConverter.java
@@ -1,3 +1,15 @@
-public class WeightConverter {
-    
+package unitconverter;
+
+public class WeightConverter implements Converter {
+
+    @Override
+    public double convert(double kg) {
+        return kg * 2.20462;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Kilograms to Pounds";
+    }
 }
+
