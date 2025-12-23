22d1c5443363b05ef799df91f7c8ed8246f47802
austin9794 <mohdumair72914@gmail.com>
2025-12-23 11:01:16 +0000

    final main

--- src/unitconverter/Main.java
+++ src/unitconverter/Main.java
@@ -1,13 +1,10 @@
 package unitconverter;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        UnitConverter.run(scanner);
-        scanner.close(); 
+        new ConverterGUI();
     }
 }
+
 
 
