5d47298c0f78e5603356fc30f7bc57acfd9e12e8
austin9794 <mohdumair72914@gmail.com>
2025-12-27 11:09:21 +0000

    fields

--- src/unitconverter/ConverterGUI.java
+++ src/unitconverter/ConverterGUI.java
@@ -6,3 +6,12 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 public class ConverterGUI extends JFrame {
+
+    private JComboBox<String> categoryBox;
+    private JComboBox<String> conversionBox;
+    private JTextField inputField;
+    private JLabel resultLabel;
+    private JButton historyButton;
+    private JButton convertButton;
+
+    
